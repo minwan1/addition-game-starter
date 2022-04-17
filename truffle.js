da61a9f5ccf3736e4d750e1b6b9bc565b17aec3c
wanjin <ansatgol@gmail.com>
2022-04-17 20:35:10 +0900

    fix build

--- truffle.js
+++ truffle.js
@@ -1,1 +1,22 @@
-// truffle.js config for klaytn.
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+const NETWORK_ID = "1001";
+const GASLIMIT = "20000000";
+const URL = `https://api.baobab.klaytn.net:8651`;
+const PRIVATE_KEY ="0x3605c915ed85daabc8559d3a8d0d74f1fc024b26c3eb742f38278cbb658cb715";
+
+module.exports = {
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+
+    klaytn: {
+      provider: new HDWalletProvider(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    },
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
-const webpack = require('webpack')
-const path = require('path')
-const fs = require('fs')
+const webpack = require("webpack");
+const path = require("path");
+const fs = require("fs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: 'development',
+  mode: "development",
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, 'dist')   
+    path: path.resolve(__dirname, "dist"),
   },
-  plugins: [   
-    // new webpack.DefinePlugin({
-    //   DEPLOYED_ADDRESS: JSON.stringify(fs.readFileSync('deployedAddress', 'utf8').replace(/\n|\r/g, "")),
-    //   DEPLOYED_ABI: fs.existsSync('deployedABI') && fs.readFileSync('deployedABI', 'utf8'),
-    // }),
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html"}])
+  node: {
+    fs: "empty",
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      DEPLOYED_ADDRESS: JSON.stringify(
+        fs.readFileSync("deployedAddress", "utf8").replace(/\n|\r/g, "")
+      ),
+      DEPLOYED_ABI:
+        fs.existsSync("deployedABI") && fs.readFileSync("deployedABI", "utf8"),
+    }),
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
   ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true }
-}
+  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+};
