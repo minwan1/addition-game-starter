629e26dc51e6214fc793d072ae26500ec6d2d07f
wanjin <ansatgol@gmail.com>
2022-04-17 23:26:33 +0900

    입금 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,23 +1,72 @@
+import Caver from "caver-js";
+import { Spinner } from "spin.js";
+
+const config = {
+  rpcUrl: 'https://api.baobab.klaytn.net:8651'
+}
+
+const cav = new Caver(config.rpcUrl);
+const agContract = new cav.klay.Contract(DEPLOYED_ABI, DEPLOYED_ADDRESS)
 
 const App = {
+  auth: {
+    accessType: 'keystore',
+    keystore: '',
+    password: ''
+  },
 
   start: async function () {
-
+    const walletFromSession = sessionStorage.getItem("walletInstance");
+    if (walletFromSession) {
+      try {
+        cav.klay.accounts.wallet.add(JSON.parse(walletFromSession));
+        this.changeUI(JSON.parse(walletFromSession));
+      } catch (e) {
+        sessionStorage.removeItem("walletInstance");
+      }
+    }
   },
 
   handleImport: async function () {
+    const fileReader = new FileReader();
+    fileReader.readAsText(event.target.files[0]);
+    fileReader.onload = (event) => {
+      try {
+        if(!this.checkValidKeystore(event.target.result)){
+          $('#message').text('유효하지 않은 keystore 파일입니다.');
+          return;
+        }
+        this.auth.keystore = event.target.result;
+        $('#message').text('keystore 통과. 비밀번호를 입력하세요.');
+        document.querySelector('#input-password').focus();
+      } catch (event) {
+        $('#message').text('유효하지 않은 keystore 파일입니다.');
+        return;
 
+      }
+    }
   },
 
   handlePassword: async function () {
-
+    this.auth.password = event.target.value;
   },
 
   handleLogin: async function () {
+    if (this.auth.accessType === 'keystore') {
+      try {
+        const privateKey = cav.klay.accounts.decrypt(this.auth.keystore, this.auth.password).privateKey;
+        this.integrateWallet(privateKey)
+
+      } catch (e) {
+        $('#message').text('비밀번호가 일치 하지 않습니다..');
+      }
+    }
 
   },
 
   handleLogout: async function () {
+    this.removeWallet();
+    location.reload();
 
   },
 
@@ -30,39 +79,98 @@ const App = {
   },
 
   deposit: async function () {
-
+    var spinner = this.showSpinner();
+    const walletInstance = this.getWallet();
+    if(walletInstance){
+      const ownerAddress = await this.callOwner()
+      if(await ownerAddress.toUpperCase() !== walletInstance.address.toUpperCase()){
+        return;
+      } else {
+        var amount = $('#amount').val();
+        if(amount){
+          agContract.methods.deposit().send({
+            from: walletInstance.address,
+            gas: '250000',
+            value: cav.utils.toPeb(amount, "KLAY") 
+          })
+          .then(function (receipt) {
+            if (receipt.transactionHash) {
+              console.log(`txHash ${receipt.transactionHash}`)
+              console.log(`(#${receipt.blockNumber})`, receipt)
+              alert(amount + " KLAY를 컨트랙에 송금했습니다.");
+              location.reload();
+            }
+          },function (error){
+            alert(error.message);
+          })
+          .always(function() {
+            spinner.stop();
+          });
+        }
+        return;
+      }
+    }
   },
 
   callOwner: async function () {
-
+    return await agContract.methods.owner().call();
   },
 
   callContractBalance: async function () {
-
+    return await agContract.methods.getBalance().call();
   },
 
   getWallet: function () {
-
+    if (cav.klay.accounts.wallet.length){
+      return cav.klay.accounts.wallet[0];
+    }
   },
 
   checkValidKeystore: function (keystore) {
+    const parsedKeystore = JSON.parse(keystore);
+    const isValidKeystore = parsedKeystore.version &&
+      parsedKeystore.id &&
+      parsedKeystore.address &&
+      parsedKeystore.keyring;
 
+      return isValidKeystore;
   },
 
   integrateWallet: function (privateKey) {
-
+    const walletInstance = cav.klay.accounts.privateKeyToAccount(privateKey);
+    cav.klay.accounts.wallet.add(walletInstance);
+    sessionStorage.setItem('walletInstance', JSON.stringify(walletInstance));
+    this.changeUI(walletInstance);
+  
   },
 
   reset: function () {
 
+    this.auth = {
+      keystore: '',
+      password: '',
+    }
   },
 
   changeUI: async function (walletInstance) {
+    $('#loginModal').modal('hide');
+    $('#login').hide();
+    $('#logout').show();
+    $('#address').append('<br>' + '<p>' + '내 계정 주소: ' + walletInstance.address + '</p>');
+    $('#contractBalance').append('<p>' + '이벤트 잔액: ' + cav.utils.fromPeb(await this.callContractBalance(), 'KLAY') + ' KLAY </p>');
 
+    const ownerAddress = await this.callOwner()
+    const result = ownerAddress === walletInstance.address
+
+    if(ownerAddress.toUpperCase() === walletInstance.address.toUpperCase()){
+      $('#owner').show()
+    }
   },
 
   removeWallet: function () {
-
+    cav.klay.accounts.wallet.clear();
+    sessionStorage.removeItem('walletInstance');
+    this.reset();
   },
 
   showTimer: function () {
@@ -70,7 +178,8 @@ const App = {
   },
 
   showSpinner: function () {
-
+    var target = document.getElementById('spin');
+    return new Spinner(opts).spin(target);
   },
 
   receiveKlay: function () {
